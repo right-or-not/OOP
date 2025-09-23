889d616716b23ff66a1f9c7f90f1770941491cd2
Knight <jue1251665735@outlook.com>
2025-09-23 21:37:39 +0800

    test agian.

--- Project/src/App.java
+++ Project/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         String word = "World";
         System.out.println("Hello, " + word + "! ! !");
+        System.out.println("I love you! ! !");
     }
 }
